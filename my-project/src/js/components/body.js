2fd8ad90645678a427654a87057d29f1c825ec91
彭伟彬 <690365189@qq.com>
2019-01-10 17:28:10 +0800

    body

--- my-project/src/js/components/body.js
+++ my-project/src/js/components/body.js
@@ -3,9 +3,18 @@ import ReactDOM from 'react-dom'
 
 export default class Body extends React.Component {
   render () {
+    let userName = 'Wrysun'
+    let boolInput = false
+    let html = "lesson&nbsp;123"
+    // 可以用uncode将html自负床汇总的特殊符号进行转码
     return (
       <div>
-        这是主体部分内容
+        <h2>这是主体部分内容</h2>
+        <p>{userName || '未登录'}</p>
+        <p><input type="button" value={userName} disabled={boolInput}/></p>
+        <p>{html}</p>
+        {/* 解析html中的特殊符号，不推荐使用 */}
+        <p dangerouslySetInnerHTML={{__html: html}}></p>
       </div>
     )
   }
