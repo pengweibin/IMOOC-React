827df91160f694d4c177bf0bb39e2c42957f86dd
彭伟彬 <690365189@qq.com>
2019-01-14 11:55:54 +0800

    state

--- my-project/src/js/components/body.js
+++ my-project/src/js/components/body.js
@@ -2,6 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 export default class Body extends React.Component {
+  constructor () {
+    super()
+    this.state = {
+      username: 'wrysun'
+    }
+  }
   componentWillMount () {
     // 页面将要加载
     console.log('will')
@@ -11,18 +17,19 @@ export default class Body extends React.Component {
     console.log('did')
   }
   render () {
-    let userName = 'Wrysun'
-    let boolInput = false
-    let html = "lesson&nbsp;123"
+    // let userName = 'Wrysun'
+    // let boolInput = false
+    // let html = "lesson&nbsp;123"
     // 可以用uncode将html自负床汇总的特殊符号进行转码
     return (
       <div>
         <h2>这是主体部分内容</h2>
-        <p>{userName || '未登录'}</p>
+        <p>{this.state.username}</p>
+        {/* <p>{userName || '未登录'}</p>
         <p><input type="button" value={userName} disabled={boolInput}/></p>
-        <p>{html}</p>
+        <p>{html}</p> */}
         {/* 解析html中的特殊符号，不推荐使用 */}
-        <p dangerouslySetInnerHTML={{__html: html}}></p>
+        {/* <p dangerouslySetInnerHTML={{__html: html}}></p> */}
       </div>
     )
   }
