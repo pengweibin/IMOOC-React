1ca9e2cccaa22782d4a9453f178044d2d271879c
彭伟彬 <690365189@qq.com>
2019-01-15 13:59:11 +0800

    内联样式

--- my-project/src/js/components/header.js
+++ my-project/src/js/components/header.js
@@ -3,8 +3,16 @@ import ReactDom from 'react-dom'
 
 export default class Header extends React.Component {
   render () {
+    //      添加样式方式一
+    const styleHeader = {
+      header: {
+        backgroundColor: '#333',
+        color: '#fff',
+        padding: '10px'
+      }
+    }
     return (
-      <header>
+      <header style={styleHeader.header} className="header">
         <h1>这里是头部</h1>
       </header>
     )
