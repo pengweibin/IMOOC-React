a447c0842b6b39e6c89c2510b623ea87a034fc2e
彭伟彬 <690365189@qq.com>
2019-01-15 14:10:08 +0800

    内联样式中表达式

--- my-project/src/js/components/header.js
+++ my-project/src/js/components/header.js
@@ -2,17 +2,28 @@ import React from 'react'
 import ReactDom from 'react-dom'
 
 export default class Header extends React.Component {
+  constructor () {
+    super()
+    this.state = {
+      minHeader: false
+    }
+  }
+  handleClickHeader () {
+    this.setState({
+      minHeader: !this.state.minHeader
+    })
+  }
   render () {
     //      添加样式方式一
     const styleHeader = {
       header: {
         backgroundColor: '#333',
         color: '#fff',
-        padding: '10px'
+        padding: this.state.minHeader ? '3px 0' : '15px 0'
       }
     }
     return (
-      <header style={styleHeader.header} className="header">
+      <header style={styleHeader.header} className="header" onClick={this.handleClickHeader.bind(this)}>
         <h1>这里是头部</h1>
       </header>
     )
