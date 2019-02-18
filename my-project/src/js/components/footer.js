88f7b7093fbfb67e2899b6496227c2821ad47e84
pengweibin <35951358+pengweibin@users.noreply.github.com>
2019-02-18 21:38:07 +0800

    'css样式快速转换jsx格式'

--- my-project/src/js/components/footer.js
+++ my-project/src/js/components/footer.js
@@ -1,13 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 // var footerCss = require('../../css/footer.css')
-import footerCss from '@/css/footer.css'
+// import footerCss from '@/css/footer.css'
 
 export default class Footer extends React.Component {
   render () {
-    console.log(footerCss)
+    const footerStyle = {
+      "minFooter": {
+        "fontSize": "14px",
+        "backgroundColor": "#333",
+        "color": "#fff",
+        "padding": "10px 3px"
+      }
+    }
+    console.log(footerStyle)
     return (
-      <footer className={footerCss.minFooter}>
+      <footer className={footerStyle.minFooter}>
         <h1>这是页脚, 一般放置版权的一些信息</h1>
       </footer>
     )
